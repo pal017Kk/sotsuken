1c334709e7ddb4cccee6f920a095b769121b7f14
【pal0】 <e1c17039@oit.ac.jp>
2020-10-26 22:44:11 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+﻿<?php
+  include 'includes/login.php';
+?>
+<html>
+<head>
+ <meta http-equiv="Content-Type" content="text/html;
+ charset=UTF-8">
+ <title>モチベーショングラフ</title>
+</head>
+<body>
+ <h1>モチベーショングラフ</h1>
+ <h2>メニュー</h2>
+ <p>
+   <a href="logout.php">ログアウト</a>
+ </p>
+  <p>
+   <a href="logout.php">研究室状況</a>
+ </p>
+  <p>
+   <a href="graph.php">グラフ</a>
+  <p>
+  <img src="graph.php">
+  </p>
+
+ </p>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,55 @@
+﻿<?php
+  session_start();
+
+  if(isset($_SESSION['id'])){
+    header('Location: index.php');
+   } else if (isset($_POST['name']) && isset($_POST['password'])){
+
+
+     $dsn = 'mysql:host=localhost;dbname=motiva;charset=utf8';
+     $user = 'motivauser';
+     $password = 'password';
+
+     try{
+       $db = new PDO($dsn, $user,$password);
+       $db->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
+
+       $stmt = $db->prepare("SELECT * FROM users WHERE name=:name AND password=:pass
+       ");
+
+       $stmt->bindParam(':name', $_POST['name'],PDO::PARAM_STR);
+       $stmt->bindParam(':pass',sha1($_POST['password']),PDO::PARAM_STR);
+
+       $stmt->execute();
+       if($row = $stmt->fetch()){
+         $_SESSION['id'] = $row['id'];
+         header('Location: index.php');
+         exit();
+       } else {
+         header('Location: login.php');
+         exit();
+       }
+     } catch(PDOException $e) {
+       die ('エラー:' .$e->getMessage());
+     }
+
+   } else {
+?>
+
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <title>卒業研究</title>
+</head>
+<body>
+  <h1>卒業研究</h1>
+
+  <h2>ログイン</h2>
+  <form action="login.php" method="post">
+    <p>ユーザー名:<input type="text" name="name"></p>
+    <p>パスワード:<input type="password" name="password"></p>
+    <p><input type="submit" value="ログイン"></p>
+  </form>
+    </body>
+</html>
+<?php } ?>
