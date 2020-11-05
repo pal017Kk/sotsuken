ee6ca9b0c68365b8d3b5a3a4261950ae69089764
【pal0】 <e1c17039@oit.ac.jp>
2020-11-05 12:31:03 +0900

    loginsitai

--- login.php
+++ login.php
@@ -1,41 +1,4 @@
-﻿<?php
-  session_start();
-
-  if(isset($_SESSION['id'])){
-    header('Location: index.php');
-   } else if (isset($_POST['name']) && isset($_POST['password'])){
-
-
-     $dsn = 'mysql:host=us-cdbr-east-02.cleardb.com;dbname=heroku_4e0bd40a54b1930;charset=utf8';
-     $user = 'b4882b891fb98a';
-     $password = '2b63b61e';
-
-     try{
-       $db = new PDO($dsn, $user,$password);
-       $db->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
-
-       $stmt = $db->prepare("SELECT * FROM users WHERE name=:name AND password=:pass
-       ");
-
-       $stmt->bindParam(':name', $_POST['name'],PDO::PARAM_STR);
-       $stmt->bindParam(':pass',sha1($_POST['password']),PDO::PARAM_STR);
-
-       $stmt->execute();
-       if($row = $stmt->fetch()){
-         $_SESSION['id'] = $row['id'];
-         header('Location: index.php');
-         exit();
-       } else {
-         header('Location: login.php');
-         exit();
-       }
-     } catch(PDOException $e) {
-       die ('エラー:' .$e->getMessage());
-     }
-
-   } else {
-?>
-
+﻿
 <html>
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
